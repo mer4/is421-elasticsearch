ec78c00277e010e44895ac55057a566847ca4692
mer4 <mer4@njit.edu>
2014-12-16 21:03:28 -0500

    Create TwitterFunctions.php

---
+++ classes/TwitterFunctions.php
@@ -0,0 +1,26 @@
+<?php
+namespace classes;
+class TwitterFunctions{
+    public static function get_field($url, $getfield, $settings){
+        $url_base= 'https://api.twitter.com/1.1/';
+        $url_base .= $url;
+        $requestMethod = 'GET';
+        $twitter = new\classes\libs\TwitterAPIExchange($settings);
+        $twitter->setGetfield($getfield);
+        $twitter->buildOauth($url_base, $requestMethod);
+        $twitter_results=$twitter->performRequest();
+        return json_decode($twitter_results, TRUE);
+        return $twitter_results;
+    }
+    public static function post_field($url, $postfield, $settings){
+        $url_base= 'https://api.twitter.com/1.1/';
+        $url_base .= $url;
+        $requestMethod = 'POST';
+        $twitter = new\classes\libs\TwitterAPIExchange($settings);
+        $twitter->buildOauth($url_base, $requestMethod);
+        $twitter->setPostfields($postfield);
+        $twitter_results=$twitter->performRequest();
+        return json_decode($twitter_results);
+    }
+}
+?>
